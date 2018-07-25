caa3dd052f9dcf4d640369157c929eba385e586a
Glaucia Isidoro <glaucia.isidoro@gtpautomation.com>
2018-07-25 10:23:09 -0300

    Input Radio no form.

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,23 +1,46 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
 import { RadioOption } from './radio-option.model';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    { 
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
-
+export class RadioComponent implements OnInit, ControlValueAccessor {
+  
   @Input() options: RadioOption[]
-
+  
   value: any
 
+  onChange: any
+  
   constructor() { }
-
+  
   ngOnInit() {
   }
-
+  
   setValue(value: any) {
     this.value = value
+    this.onChange(this.value)
   }
-
+  
+  writeValue(obj: any): void {
+    this.value = obj
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
 }
