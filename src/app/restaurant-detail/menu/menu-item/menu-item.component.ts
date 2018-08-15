a4dc7472e30d7dbb06811289e41a66d9aa48c4b1
Glaucia Isidoro <glaucia.isidoro@gtpautomation.com>
2018-08-15 15:39:41 -0300

    Menu animations.

--- src/app/restaurant-detail/menu/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu/menu-item/menu-item.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { MenuItem } from './menu-item.model';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translateY(-30px)'}),
+        animate('500ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
+
+  menuState = 'ready';
 
   @Input() menuItem: MenuItem;
   @Output() add = new EventEmitter()
