3aab0f6a7fe02ca46469fb8ab32a22c1bd4072a2
Glaucia Isidoro <glaucia.isidoro@gtpautomation.com>
2018-08-15 13:13:45 -0300

    snackbar animations options

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { state, transition, animate, trigger, style } from '@angular/animations';
 
 @Component({
   selector: 'mt-snackbar',
   templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.css']
+  styleUrls: ['./snackbar.component.css'],
+  animations: [
+    trigger('snack-visibility', [
+      state('hidden', style({
+        opacity: 0,
+        bottom: 0
+      })),
+      state('visible', style({
+        opacity: 1,
+        bottom: '30px'
+      })),
+      transition('hidden => visible', animate('500ms 0s ease-in')),
+      transition('visible => hidden', animate('500ms 0s ease-out'))
+    ])
+  ]
 })
 export class SnackbarComponent implements OnInit {
 
   message: string = 'snackbar message';
+  snackVisibility: string = 'hidden';
 
   constructor() { }
 
