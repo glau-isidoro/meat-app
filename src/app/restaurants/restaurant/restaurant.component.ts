501831faa91d8d3a9e9c1a5595dc692de26560af
Glaucia Isidoro <glaucia.isidoro@gtpautomation.com>
2018-08-15 15:22:17 -0300

    restaurantes animations.

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { state, transition, animate, trigger, style } from '@angular/animations';
 import { Restaurant } from './restaurant.model';
 
 @Component({
   selector: 'mt-restaurant',
-  templateUrl: './restaurant.component.html'
+  templateUrl: './restaurant.component.html',
+  animations: [
+    trigger('restaurantAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translate(-30px, -10px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class RestaurantComponent implements OnInit {
+
+  restaurantState = 'ready';
 
   @Input() restaurant: Restaurant;
 
