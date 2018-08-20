d0afcae2c00539bc3bfd440d613025b3335c4bf8
Glaucia Isidoro <glaucia.isidoro@gtpautomation.com>
2018-08-20 16:42:16 -0300

    Login Logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './security/login/login.component';
+import { UserDetailComponent } from './header/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { LoginComponent } from './security/login/login.component';
     ReviewsComponent,
     OrderSummaryComponent,
     NotFoundComponent,
-    LoginComponent
+    LoginComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -4,19 +4,27 @@ import { Observable } from 'rxjs/Observable';
 import { MEAT_API } from '../../app.api';
 import { User } from './user.model';
 import 'rxjs/add/operator/do'
-import { Router } from '@angular/router';
+import 'rxjs/add/operator/filter'
+import { Router, NavigationEnd } from '@angular/router';
 
 @Injectable()
 export class LoginService {
 
     user: User
+    lastUrl: string
 
-    constructor(private http: HttpClient,
-                private router: Router) {}
+    constructor(
+        private http: HttpClient,
+        private router: Router
+    ) {
+        this.router.events
+            .filter(e => e instanceof NavigationEnd)
+            .subscribe( (e: NavigationEnd) => this.lastUrl = e.url)
+    }
 
     login(email: string, password: string): Observable<User> {
         return this.http
-            .post<User>(`${MEAT_API}/login`, {email: email, password: password})
+            .post<User>(`${MEAT_API}/login`, { email: email, password: password })
             .do(user => this.user = user)
     }
 
@@ -24,8 +32,12 @@ export class LoginService {
         return this.user !== undefined
     }
 
-    handleLogin(path?: string) {
+    handleLogin(path: string = this.lastUrl) {
         this.router.navigate(['/login', btoa(path)])
+    }
+
+    logout() {
+        this.user = undefined
     }
 
 }
