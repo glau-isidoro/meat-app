ccc980f7fdfbd553f9cfe12a87096d4ef0b4957f
Glaucia Isidoro <glaucia.isidoro@gtpautomation.com>
2018-08-20 15:15:20 -0300

    RouteGuard - CanActivate

--- src/app/security/loggedin.guard.ts
+++ src/app/security/loggedin.guard.ts
@@ -1,18 +1,25 @@
-import { CanLoad, Route } from '@angular/router';
+import { CanLoad, Route, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { LoginService } from './login/login.service';
 
 @Injectable()
-export class LoggedInGuard implements CanLoad {
+export class LoggedInGuard implements CanLoad, CanActivate {
 
   constructor(private loginService: LoginService) {}
 
   canLoad(route: Route): boolean {
+    return this.checkAuthentication(route.path)
+  }
+
+  canActivate(activatedRoute: ActivatedRouteSnapshot, routerState: RouterStateSnapshot): boolean {
+    return this.checkAuthentication(activatedRoute.routeConfig.path)
+  }
+
+  checkAuthentication(path: string): boolean {
     const loggedIn = this.loginService.isLoggedIn()
     if(!loggedIn) {
-      this.loginService.handleLogin(`${route.path}`)
+      this.loginService.handleLogin(`${path}`)
     }
     return loggedIn
   }
-
 }
