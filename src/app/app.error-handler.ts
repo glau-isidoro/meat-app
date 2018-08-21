8f98c67a27183d711d23b0ddb822928f09c48f36
Glaucia Isidoro <glaucia.isidoro@gtpautomation.com>
2018-08-21 14:10:08 -0300

    ErrorHandler

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,16 +1,34 @@
 import { HttpErrorResponse } from '@angular/common/http'
-import { Observable } from 'rxjs/Observable'
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { NotificationService } from './shared/messages/notification.service';
+import { LoginService } from './security/login/login.service';
 
-export class ErrorHandler {
-    static handleError(error: HttpErrorResponse | any) {
-        let errorMessage: string
-        if(error instanceof HttpErrorResponse) {
-            const body = error.error
-            errorMessage = `Erro ${error.status} ao acessar a url ${error.url} - ${error.statusText} - ${body}`
-        } else {
-            errorMessage = error.toString()
-        }
-        console.log(errorMessage)
-        return Observable.throw(errorMessage)
+@Injectable()
+export class ApplicationErrorHandler extends ErrorHandler {
+
+  constructor(private notification: NotificationService, private inject: Injector) {
+    super()
+    //quando customizamos um construtor de uma classe que herda outra,
+    //o construtor da classe super deixa de ser chamado. Por isso o super().
+  }
+
+  handleError(errorResponse: HttpErrorResponse | any) {
+    //super.handleError(errorResponse) já faz mais ou menos o que nosso
+    //antigo handleError fazia
+    if (errorResponse instanceof HttpErrorResponse) {
+      const message = errorResponse.error.message
+      switch (errorResponse.status) {
+        case 401:
+          this.inject.get(LoginService).handleLogin()
+          break
+        case 403:
+          this.notification.notify(message || 'Não autorizado.')
+          break
+        case 404:
+          this.notification.notify(message || 'Recurso não encontrado.')
+          break
+      }
     }
+    super.handleError(errorResponse)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule, LOCALE_ID, ErrorHandler } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
@@ -20,6 +20,7 @@ import { SharedModule } from './shared/shared.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './security/login/login.component';
 import { UserDetailComponent } from './header/user-detail/user-detail.component';
+import { ApplicationErrorHandler } from './app.error-handler';
 
 @NgModule({
   declarations: [
@@ -46,7 +47,8 @@ import { UserDetailComponent } from './header/user-detail/user-detail.component'
     SharedModule.forRoot()
   ],
   providers: [
-    {provide: LOCALE_ID, useValue: 'pt-BR'} //i18n pro currency mostrar R$1,00 ao invés de R$1.00
+    {provide: LOCALE_ID, useValue: 'pt-BR'}, //i18n pro currency mostrar R$1,00 ao invés de R$1.00
+    {provide: ErrorHandler, useClass: ApplicationErrorHandler}
   ],
   bootstrap: [AppComponent]
 })
